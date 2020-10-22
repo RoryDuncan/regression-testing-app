fdc1d65af3f1098212bd6d9dab3ba30d09e15081
Rory Duncan <robust.rory@gmail.com>
2020-10-22 00:59:31 -0700

    Add sprint details

--- src/App.svelte
+++ src/App.svelte
@@ -7,46 +7,61 @@
   import TestInstructions from "@components/TestInstructions.svelte";
   import FeatureTestHeading from "@components/FeatureTestHeading.svelte";
   import Overview from "@components/Overview.svelte";
+  import SprintDetails from "@components/SprintDetails.svelte";
 
   const gifLink = "https://media.giphy.com/media/cyd9pp3G33l1CF8sA8/giphy.gif";
 </script>
 
 <style>
-	.split-view {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		grid-gap: 1rem;
-	}
+  .split-view {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+  }
+
+  .container {
+    padding: 1rem 1rem;
+    max-width: 90rem;
+    margin: 0 auto;
+  }
 </style>
 
-<Overview />
+<SprintDetails />
 
-<FeatureTestHeading />
-{#each items as item, i}
-  <FeatureTest {...item} bind:status={item.status}>
-    <div class="split-view">
-      <div class="test-details">
-        <TestHeader configURL={item.configURL} testURL={item.testURL} />
-        <TestSteps steps={item.steps} />
-        <TestInstructions instructions={item.instructions} />
+<div class="container">
+  <Overview />
+
+  <FeatureTestHeading />
+  {#each items as item, i}
+    <FeatureTest {...item} bind:status={item.status}>
+      <TestHeader configURL={item.configURL} testURL={item.testURL} />
+      <div class="split-view">
+        <div class="test-details">
+
+          <TestSteps steps={item.steps} />
+          <TestInstructions instructions={item.instructions} />
+        </div>
+        <div class="test-feedback">
+          <h5>Gif / Video</h5>
+          <p>
+            <img src={gifLink} alt="frog" />
+          </p>
+          <div class="text-right">
+            <button>Upload New</button>
+          </div>
+          <h5>Issues</h5>
+          <textarea value="None" />
+
+          {#if item.status === TEST_STATE.REGRESSED}
+            <h5>Link to Regression Task</h5>
+            <input
+              type="text"
+              value="https://worktruck.visualstudio.com/WTS-Suite/_workitems/edit/" />
+          {/if}
+
+        </div>
       </div>
-      <div class="test-feedback">
-	  	<h5>Gif / Video</h5>
-		<p>
-			<img src={gifLink} alt="frog" />
-		</p>
-		<div class="text-right">
-			<button>Upload New</button>
-		</div>
-        <h5>Issues</h5>
-		<textarea value="None"></textarea>
+    </FeatureTest>
+  {/each}
 
-		{#if item.status === TEST_STATE.REGRESSED}
-			<h5>Link to Regression Task</h5>
-			<input type="text" value="https://worktruck.visualstudio.com/WTS-Suite/_workitems/edit/"/>
-		{/if}
-
-      </div>
-    </div>
-  </FeatureTest>
-{/each}
+</div>
---
+++ src/components/SprintDetails.svelte
@@ -0,0 +1,31 @@
+<style>
+  .sprint-layout {
+    display: grid;
+    grid-template-columns: 10rem 20rem 10rem;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #eee;
+    background-color: #f2f2f2;
+    padding: 1rem;
+  }
+</style>
+
+<div class="sprint-layout">
+  <button class="previous">
+    Previous Sprint:
+    <br />
+    Gen14
+  </button>
+  <div class="current">
+    <h1>Sprint gen16</h1>
+    <h2>Team 1 <span class="muted">|</span> using .ci1</h2>
+    <div>
+      <a>Go to gen16 in VSTS</a>
+    </div>
+  </div>
+  <button class="next">
+    Next Sprint:
+    <br />
+    Create
+  </button>
+</div>
